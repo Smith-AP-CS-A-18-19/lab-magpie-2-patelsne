376286210dae4c946785ad737e0f3814ffa44ef6
Sneha Patel <Supersneha@patelsne-19-sp3.accounts.berkeleyprep.org>
2018-10-04 14:51:03 -0400

    here's the editted code

--- Magpie2.java
+++ Magpie2.java
@@ -28,17 +28,39 @@ public class Magpie2 {
 	 */
 	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
-			response = "Why so negative?";
-		} else if (statement.indexOf("mother") >= 0
-				   || statement.indexOf("father") >= 0
-				   || statement.indexOf("sister") >= 0
-				   || statement.indexOf("brother") >= 0)
+		if (statement.indexOf("no") >= 0) { //index finds where a substring starts
+			response = "Why so negative?";  //if the substring isn't there, then
+		} else if (statement.indexOf("mother") >= 0 //it returns -1
+				   || statement.indexOf("father") >= 0 //thus, these .indexOf() methods
+				   || statement.indexOf("sister") >= 0 //say "if something is in the
+				   || statement.indexOf("brother") >= 0) // phrase, do blank"
 		{
 			response = "Tell me more about your family.";
-		} else {
+		} else if (statement.indexOf("cat") >=0
+						|| statement.indexOf("dog") >=0
+						|| statement.indexOf("fish") >=0)
+						{
+							response = "Tell me more about your pets.";
+			} else if (statement.indexOf("Mr. Smith") >= 0)
+					{
+							response = "He sounds like a good teacher.";
+			} else if (statement.indexOf("music") >= 0
+								|| statement.indexOf("song") >= 0)
+					{
+							response = "Oh! You like music? My favorite song is All Star!";
+			} else if (statement.indexOf("food") >= 0)
+				{
+					response = "stop i'm getting hungry :(";
+			} else if (statement.indexOf("pizza") >= 0)
+				{
+					response = "lol I love pizza";
+				}
+			 else if (statement.trim().length() == 0)
+				{
+					response = "say something, please";
+			} else {
 			response = getRandomResponse();
-		}
+			}
 		return response;
 	}
 
@@ -47,7 +69,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -60,6 +82,10 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "wow you're super cool";
+		} else if (whichResponse == 5) {
+			response = "huh that's pretty neat";
 		}
 
 		return response;
